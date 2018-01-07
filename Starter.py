34990c6a3dd076aa6fdf8451a71d4b69e2114bb6
pzhao <424107420@qq.com>
2018-01-08 01:24:37 +0800

    [edit] 修改数据生成excel

--- Starter.py
+++ Starter.py
@@ -3,48 +3,19 @@
 import os
 import time
 import xlwt
-from enum import Enum, unique
 
-@unique
-class fontUnderline(Enum):
-    xlwt.Font.UNDERLINE_NONE = 1
-    xlwt.Font.UNDERLINE_SINGLE = 2
-    xlwt.Font.UNDERLINE_SINGLE_ACC = 3
-    xlwt.Font.UNDERLINE_DOUBLE = 4
-    xlwt.Font.UNDERLINE_DOUBLE_ACC = 5
-
-@unique
-class fontEscapement(Enum):
-    xlwt.Font.ESCAPEMENT_NONE = 1
-    xlwt.Font.ESCAPEMENT_SUPERSCRIPT = 2
-    xlwt.Font.ESCAPEMENT_SUBSCRIPT = 3
-
-@unique
-class fontFamily(Enum):
-    xlwt.Font.FAMILY_NONE = 1
-    xlwt.Font.FAMILY_ROMAN = 2
-    xlwt.Font.FAMILY_SWISS = 3
-    xlwt.Font.FAMILY_MODERN = 4
-    xlwt.Font.FAMILY_SCRIPT = 5
-    xlwt.Font.FAMILY_DECORATIVE = 6
-
-@unique
-class cellBorders(Enum):
-    xlwt.Borders.NO_LINE = 1
-    xlwt.Borders.THIN = 2
-    xlwt.Borders.MEDIUM = 3
-    xlwt.Borders.DASHED = 4
-    xlwt.Borders.DOTTED = 5
-    xlwt.Borders.THICK = 6
-    xlwt.Borders.DOUBLE = 7
-    xlwt.Borders.HAIR = 8
-    xlwt.Borders.MEDIUM_DASHED = 9
-    xlwt.Borders.THIN_DASH_DOTTED = 10
-    xlwt.Borders.MEDIUM_DASH_DOTTED = 11
-    xlwt.Borders.THIN_DASH_DOT_DOTTED = 12
-    xlwt.Borders.MEDIUM_DASH_DOT_DOTTED = 13
-    xlwt.Borders.SLANTED_MEDIUM_DASH_DOTTED = 14
-
+fontUnderline = [xlwt.Font.UNDERLINE_NONE, xlwt.Font.UNDERLINE_SINGLE, xlwt.Font.UNDERLINE_SINGLE_ACC,
+                 xlwt.Font.UNDERLINE_DOUBLE, xlwt.Font.UNDERLINE_DOUBLE_ACC]
+fontEscapement = [xlwt.Font.ESCAPEMENT_NONE, xlwt.Font.ESCAPEMENT_SUPERSCRIPT, xlwt.Font.ESCAPEMENT_SUBSCRIPT]
+fontFamily = [xlwt.Font.FAMILY_NONE, xlwt.Font.FAMILY_ROMAN, xlwt.Font.FAMILY_SWISS, xlwt.Font.FAMILY_MODERN,
+              xlwt.Font.FAMILY_SCRIPT, xlwt.Font.FAMILY_DECORATIVE]
+cellBorders = [xlwt.Borders.NO_LINE, xlwt.Borders.THIN, xlwt.Borders.MEDIUM, xlwt.Borders.DASHED, xlwt.Borders.DOTTED,
+               xlwt.Borders.THICK, xlwt.Borders.DOUBLE, xlwt.Borders.HAIR, xlwt.Borders.MEDIUM_DASHED,
+               xlwt.Borders.THIN_DASH_DOTTED, xlwt.Borders.MEDIUM_DASH_DOTTED, xlwt.Borders.THIN_DASH_DOT_DOTTED,
+               xlwt.Borders.MEDIUM_DASH_DOT_DOTTED, xlwt.Borders.SLANTED_MEDIUM_DASH_DOTTED]
+colorIndex = { "Black": 0, "White" : 1, "Red" : 2, "Green" : 3, "Blue" : 4, "Yellow" : 5, "Magenta" : 6,
+               "Cyan" : 7, "Maroon" : 16, "Dark Green" : 17, "Dark Blue" : 18, "Dark Yellow" : 19,
+               "Dark Magenta" : 20, "Teal" : 21, "Light Gray" : 22, "Dark Gray" : 23 }
 
 def getResultFromDB(host, username, password, db, sqlStr):
     # 连接数据库，执行sql
@@ -62,7 +33,7 @@ def createWorkbook():
 
 def saveWorkbook(workbook):
     excelTime = time.strftime("%Y%m%d")
-    workbook.save(excelTime + "cert.xlsx")
+    workbook.save(excelTime + "cert.xls")
     print("Save OK")
 
 
@@ -101,27 +72,27 @@ def setStyle(fontHeight, colorIndex, fontBold, fontUnderline, fontEscapement, fo
     return style
 
 
-def writeData(sheet, rowKey, dataValue, style):
+def writeData(sheet, rowKey, dataValue, titleStyle, keyStyle, valueStyle):
     rows = len(rowKey)
 
     # 设置列的宽度
     sheet.col(0).width = 2000
-    sheet.col(1).width = 4000
+    sheet.col(1).width = 5000
     sheet.col(2).width = 20000
     sheet.col(3).width = 6000
-    sheet.col(4).width = 4000
+    sheet.col(4).width = 5000
     sheet.col(5).width = 20000
     sheet.col(6).width = 2000
 
-
+    sheet.write_merge(1, 1, 1, 2, "  12  （2016）  苏州市  不动产权第  201712310001  号  ", titleStyle)
     for i in range(rows - 1):
-        sheet.write(i + 2, 1, rowKey[i], style)
-        sheet.write(i + 2, 2, dataValue[i], style)
-    sheet.write_merge(rows - 1, rows + 2, 1, 1, rowKey[rows - 1], style)
-    sheet.write_merge(rows - 1, rows + 2, 2, 2, dataValue[rows - 1], style)
+        sheet.write(i + 2, 1, rowKey[i], keyStyle)
+        sheet.write(i + 2, 2, dataValue[i], valueStyle)
+    sheet.write_merge(rows + 1, rows + 4, 1, 1, rowKey[rows - 1], keyStyle)
+    sheet.write_merge(rows + 1, rows + 4, 2, 2, dataValue[rows - 1], valueStyle)
 
-    sheet.write_merge(2, rows + 2, 4, 5, "", style)
-    return sheet
+    sheet.write_merge(1, 1, 4, 5, "附    记", titleStyle)
+    sheet.write_merge(2, rows + 4, 4, 5, "1、房屋多人共同所有；", valueStyle)
 
 
 if __name__ == "__main__":
@@ -134,15 +105,11 @@ if __name__ == "__main__":
     rowKey = ['权利人', '共有情况', '坐落', '不动产单元号', '权利类型', '权利性质', '用途', '面积', '使用期限', '权利其他情况']
     dataValue = ['赵鹏', '共同共有', '彭义里40号', '320508 024003 GB01700 12457896', '国有建设用地使用权/房屋（构建物）所有权', '划拨', '城镇住宅用地/成套住宅', '分摊土地面积24.80平方米/房屋建筑面积63.7平方米', '截止到2030年12月31日', '房屋结构：混合']
 
-    colorIndex = { 0 : "Black", 1 : "White", 2 : "Red", 3 : "Green", 4 : "Blue", 5 : "Yellow", 6 : "Magenta", 7 : "Cyan", 16 : "Maroon", 17 : "Dark Green", 18 : "Dark Blue", 19 : "Dark Yellow", 20 : "Dark Magenta", 21 : "Teal", 22 : "Light Gray", 23 : "Dark Gray" }
-
-    titleStyle = setStyle()
-    keyStyle = setStyle(260, colorIndex[0], True, fontUnderline())
+    titleStyle = setStyle(350, colorIndex["Black"], True, fontUnderline[0], fontEscapement[0], fontFamily[0], cellBorders[0])
+    keyStyle = setStyle(300, colorIndex["Black"], True, fontUnderline[0], fontEscapement[0], fontFamily[0], cellBorders[2])
+    valueStyle = setStyle(300, colorIndex["Black"], False, fontUnderline[0], fontEscapement[0], fontFamily[0], cellBorders[2])
 
     workbook = createWorkbook()
     sheet = createSheet(workbook)
-    writeData(sheet, rowKey, dataValue, style)
-
-
-
-
+    writeData(sheet, rowKey, dataValue, titleStyle, keyStyle, valueStyle)
+    saveWorkbook(workbook)
